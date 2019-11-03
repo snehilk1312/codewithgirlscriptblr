082446fd2f51df3fd5e2cc08fd12d564e0b389c5
snehil <44499005+snehilk1312@users.noreply.github.com>
2019-11-04 02:17:26 +0530

    Create merging_soln_new.py

---
+++ Medium/19-11-03-Merge Intervals/merging_soln_new.py
@@ -0,0 +1,12 @@
+import ast
+m=list()
+list_str=input()
+my_list=ast.literal_eval(list_str)
+my_list=sorted(my_list)
+for i in range(len(my_list)-1):
+    if my_list[i][1]>=my_list[i+1][0]:
+        my_list[i+1]=[min(*my_list[i],*my_list[i+1]),max(*my_list[i],*my_list[i+1])]
+        my_list[i]=['a','b']
+while my_list.count(['a','b']):
+    my_list.remove(['a','b'])
+print(my_list)
